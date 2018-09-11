89a660cf318bb0fa30873fe77048fdc7851897f9
Lucas Silva <lucasluizss@live.com>
2018-09-11 20:37:37 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<stdio.h>
+#include<string.h>
+#include"scan.h"
+
+//using namespace std;
+//extern Tk *getTokens(char *nome);
+
+/*********************************
+ * Nome: main
+ * desc:
+ * returno:
+ ********************************/
+int main(int argc, char *argv[])
+{
+    printf("teste");
+
+    Tk *listToken;
+
+    listToken = getTokens("arquivoe1");
+
+    return 0;
+}
