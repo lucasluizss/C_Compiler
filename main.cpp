b4f030af98e0dc7fe951ee3a63d6e2e7ef1c6a7a
lucasluizss <lucasluizss@live.com>
2018-10-10 22:13:00 -0300

    Revert "Aula 09-10 / Análise léxica"
    This reverts commit 3901f383271c9384627241eac06dbe5e158a0479.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
-#include<iostream>
-#include<list>
-#include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 #include"scan.h"
 
-using namespace std;
+//using namespace std;
 //extern Tk *getTokens(char *nome);
-Tk* tokens;
-list <Tk*> simbolos;
-list <Tk*> pReservadas;
-list <Tk*> listId;
-int contId;
 
-void analiseLexica();
-void loadId();
-void loadPr();
 /*********************************
  * Nome: main
  * desc:
@@ -23,9 +12,7 @@ void loadPr();
  ********************************/
 int main(int argc, char *argv[])
 {
-    //simbolos.push
-
-    contId = 0;
+    Tk *Lista;
     char nome[50];// = "arquivo.c";
 
     if(argc==2){
@@ -38,48 +25,6 @@ int main(int argc, char *argv[])
 
     Lista = getTokens(nome);
     exibeTk(Lista);
-    analiseLexica();
-    listId.clear();
 
     return 0;
-}
-
-void analiseLexica(){
-    Tk* list;
-    // list<Tk*>::iterator it;
-
-    // for(it = simbolos.front(); it != NULL; it++){
-
-    // }
-
-    for(list = tokens; list != NULL; list=list->prox){
-
-    }
-}
-
-void loadId(){
-    char nome[][10] = {"int", "char", "float", "double", "short", "long"}
-    Tk* l;
-    
-    for(int = 0; i < 6; i++){
-        l = (Tk*) malloc(sizeof(Tk));
-        l->nome = (char*)malloc(sizeof(char)*strlen(nome[0]));
-        strcpy(l->nome, nome[i]);
-        contId++;
-        listId.push_front(l);
-        //pReservadas
-    }
-}
-
-void loadPr(){
-    char nome[][10] = {"do", "for", "while", "switch", "else", "if"}
-    Tk* l;
-    
-    for(int = 0; i < 6; i++){
-        l = (Tk*) malloc(sizeof(Tk));
-        l->nome = (char*)malloc(sizeof(char)*strlen(nome[0]));
-        strcpy(l->nome, nome[i]);
-        contId++;
-        //pReservadas
-    }
 }
